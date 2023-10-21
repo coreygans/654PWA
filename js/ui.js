5295c9f2947feb60370ee1ace30ae569a032a95b
Corey <coreygans@gmail.com>
2023-10-21 11:17:37 -0500

    First commit to git

---
+++ js/ui.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //Nav Menu
+  const menus = document.querySelectorAll(".side-menu");
+  M.Sidenav.init(menus, { edge: "left" });
+  // var elems = document.querySelectorAll('.sidenav');
+  // var instances = M.Sidenav.init(elems, { edge: 'left', draggable: false });
+
+  // Add Transactions
+  const forms = document.querySelectorAll(".side-form");
+  M.Sidenav.init(forms, { edge: "right" });
+  //Select menu
+  var elems = document.querySelectorAll("select");
+  var instances = M.FormSelect.init(elems, {
+    dropdownOptions: {
+      alignment: "left",
+      coverTrigger: false,
+    },
+  });
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems);
+});
